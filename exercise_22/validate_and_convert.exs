66949a092960021e5305aeb3998a9edd789f8145
Nicolas Oga <2112.oga@gmail.com>
2021-04-27 18:43:27 -0300

    First working version of ex. 22

--- exercise_22/validate_and_convert.exs
+++ exercise_22/validate_and_convert.exs
@@ -5,9 +5,6 @@ defmodule ValidateAndConvert do
     |> and_maybe(&validate/1)
   end
 
-  defp and_maybe({ :ok, value }, func), do: func.(value)
-  defp and_maybe(anything_else, _func), do: anything_else
-
   defp convert(value) when is_binary(value) do
     case Integer.parse(value) do
       {int, _rest} -> {:ok, int}
@@ -18,14 +15,11 @@ defmodule ValidateAndConvert do
   defp convert(value) when is_integer(value), do: { :ok, value }
 
   defp validate(value) when is_integer(value) and value > 18 and value < 150 do
-    debug(value)
     { :ok, value } 
   end
 
   defp validate(value) when is_integer(value), do: { :error, "Integer #{value} is out of bounds. It should be greater than 18 and less than 150." } 
 
-  defp debug(value) do
-    IO.inspect(value)
-    value
-  end
+  defp and_maybe({ :ok, value }, func), do: func.(value)
+  defp and_maybe(anything_else, _func), do: anything_else
 end
